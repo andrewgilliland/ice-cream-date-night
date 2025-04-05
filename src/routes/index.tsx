85ff8e6a9aad61c085a060b9d7e31f28ab2ff6a9
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-04-05 08:36:14 -0500

    feat: updates HomePage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,18 @@
 import { A } from "@solidjs/router";
 import Counter from "~/components/Counter";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
+      <div class="flex flex-col items-center justify-center mt-20 mx-auto max-w-md">
+        <h1 class="font-bold text-2xl text-pink-300">
+          it's time for an ice cream date night!
+        </h1>
+        <p class="text-gray-100 mt-4">
+          There are a total of 18 flavors to fall in love with. which ones will
+          you love a litte & which ones will you love a lot?
+        </p>
+      </div>
     </main>
   );
 }
